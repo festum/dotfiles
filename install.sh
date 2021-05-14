b228f100ca88c06415ffc0aaf2c361c16e0316c3
Festum Qin <festum@g.pl>
2021-05-14 17:28:36 +0200

    add: vimrc

--- install.sh
+++ install.sh
@@ -7,7 +7,11 @@ link() {
 	TARGET_DIR=$HOME
 	if [[ -n $2 ]]; then TARGET_DIR=$2; fi
     if test ! -f "$TARGET_DIR/$FILENAME"; then
-		echo "linking to $TARGET_DIR/$FILENAME"
+		echo "linking $TARGET_DIR/$FILENAME"
+		ln -s $DOTFILES_DIR/$FILENAME $TARGET_DIR/$FILENAME
+	elif [[ ! -L "$TARGET_DIR/$FILENAME" ]]; then
+		echo "replacing $TARGET_DIR/$FILENAME"
+		mv -f $TARGET_DIR/$FILENAME $TARGET_DIR/$FILENAME.bak
 		ln -s $DOTFILES_DIR/$FILENAME $TARGET_DIR/$FILENAME
 	fi
 }
@@ -18,6 +22,7 @@ link .tmux.conf.local
 link .gitconfig
 link .gitignore_global
 link .inputrc
+link .vimrc
 link .wgetrc
 [ ! -d $HOME/.config/kitty ] && mkdir -p $HOME/.config/kitty
 link kitty.conf $HOME/.config/kitty
