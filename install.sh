eed921a2e086a30bf223c5bb51fb5bf21c7e8779
Festum Qin <festum@g.pl>
2022-05-05 13:22:48 +0200

    rc(helix): add config and link

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 DOTFILES_DIR=$(pwd)
 
 link() {
-	FILENAME=$1
-	TARGET_DIR=$HOME
-	if [[ -n $2 ]]; then TARGET_DIR=$2; fi
+  FILENAME=$1
+  TARGET_DIR=$HOME
+  if [[ -n $2 ]]; then TARGET_DIR=$2; fi
   if test ! -f "$TARGET_DIR/$FILENAME"; then
     echo "linking $TARGET_DIR/$FILENAME"
     ln -s $DOTFILES_DIR/$FILENAME $TARGET_DIR/$FILENAME
@@ -13,7 +13,7 @@ link() {
     echo "replacing $TARGET_DIR/$FILENAME"
     mv -f $TARGET_DIR/$FILENAME $TARGET_DIR/$FILENAME.bak
     ln -s $DOTFILES_DIR/$FILENAME $TARGET_DIR/$FILENAME
-	fi
+  fi
 }
 
 link .bash_aliases
@@ -25,6 +25,7 @@ link .inputrc
 link .vimrc
 link .wgetrc
 [ ! -d $HOME/.config/kitty ] && mkdir -p $HOME/.config/kitty && link kitty.conf $HOME/.config/kitty
+[ ! -d $HOME/.config/helix ] && mkdir -p $HOME/.config/helix && ln -s helix/config.toml $HOME/.config/helix 
 [ ! -d $HOME/.config/nvim/lua/custom ] && mkdir -p $HOME/.config/nvim/lua && ln -s $DOTFILES_DIR/nvchad/custom $HOME/.config/nvim/lua
 [ -d $HOME/.termux ] && link termux.properties $HOME/.termux && wget -O ~/.termux/colors.properties https://raw.githubusercontent.com/dracula/termux/master/colors.properties
 
