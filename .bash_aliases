2e8dc1089d0f4aed8af376f708b37036ccfe8fc1
Festum Qin <festum@g.pl>
2022-09-09 14:52:18 +0200

    alias: ed omit electron errors

--- .bash_aliases
+++ .bash_aliases
@@ -469,7 +469,7 @@ alias i2='convert -density 300 -quality 100'
 # -------------------------------------------------------------------
 ! is_runnable code && is_runnable code-oss && alias code=code-oss
 ! is_runnable code && is_runnable codium && alias code=codium
-is_runnable code && alias ed='f() { file=${1:-.}; [ "$file" != "." ] && file="-r ${file}"; code $file; unset -f f;}; f'
+is_runnable code && alias ed='f() { file=${1:-.}; [ "$file" != "." ] && file="-r ${file}"; code $file 2>/dev/null; unset -f f;}; f'
 is_runnable code && alias vc=code
 is_runnable nvim && alias nv=nvim
 ! is_runnable pm && is_runnable homebrew && alias pm='sudo homebrew'
--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ link .inputrc
 link .vimrc
 link .wgetrc
 [ ! -d $HOME/.config/kitty ] && mkdir -p $HOME/.config/kitty && link kitty.conf $HOME/.config/kitty
-[ ! -d $HOME/.config/helix ] && mkdir -p $HOME/.config/helix && ln -s helix/config.toml $HOME/.config/helix 
-[ ! -d $HOME/.config/nvim/lua/custom ] && mkdir -p $HOME/.config/nvim/lua && ln -s $DOTFILES_DIR/nvchad/custom $HOME/.config/nvim/lua
+[ ! -d $HOME/.config/helix ] && mkdir -p $HOME/.config/helix && ln -s helix/config.toml $HOME/.config/helix
+[ ! -d $HOME/.config/nvim ]
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && rm -rf $HOME/.config/nvim/lua/custom && ln -s $DOTFILES_DIR/nvchad/custom $HOME/.config/nvim/lua
 [ -d $HOME/.termux ] && link termux.properties $HOME/.termux && wget -O ~/.termux/colors.properties https://raw.githubusercontent.com/dracula/termux/master/colors.properties
-
