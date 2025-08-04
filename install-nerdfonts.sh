6e7b0b6c0d903b2bf8bd29c88830e0643f4fae96
Festum Qin <festum@g.pl>
2025-08-04 13:10:16 +0200

    config: zed

--- install-nerdfonts.sh
+++ install-nerdfonts.sh
@@ -2,14 +2,13 @@
 
 declare -a fonts=(
   AnonymousPro
-	FiraCode
-	Hack
-	InconsolataGo
-	NerdFontsSymbolsOnly
-	UbuntuMono
+  FiraCode
+  Hack
+  InconsolataGo
+  NerdFontsSymbolsOnly
+  UbuntuMono
 )
 
-version='3.0.2'
 fonts_dir="${HOME}/.local/share/fonts"
 
 if [[ ! -d "$fonts_dir" ]]; then
@@ -18,9 +17,9 @@ fi
 
 for font in "${fonts[@]}"; do
     zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${zip_file}"
     echo "Downloading $download_url"
-  	wget -q "$download_url" -O /tmp/$zip_file
+    wget -q "$download_url" -O "/tmp/$zip_file"
     unzip -o "/tmp/$zip_file" -d "$fonts_dir" -x "*.otf" -x "*.txt" -x "*.md" -x "LICENSE"
     rm -f "/tmp/$zip_file"
 done
--- install.sh
+++ install.sh
@@ -3,52 +3,102 @@
 DOTFILES_DIR=$(pwd)
 
 backup() {
-  TARGET_PATH=$1
-  ([[ -f $TARGET_PATH ]] ||[[ -d $TARGET_PATH ]]) && mv $TARGET_PATH ${TARGET_PATH}.bak
-}
-
-link() {
-  FILENAME=$1
-  TARGET_DIR=$HOME
-  if [[ -n $2 ]]; then TARGET_DIR=$2; fi
-  if test ! -f "$TARGET_DIR/$FILENAME"; then
-    echo "linking $TARGET_DIR/$FILENAME"
-    ln -s $DOTFILES_DIR/$FILENAME $TARGET_DIR/$FILENAME
-  elif [[ ! -L "$TARGET_DIR/$FILENAME" ]]; then
-    echo "replacing $TARGET_DIR/$FILENAME"
-    backup $TARGET_DIR/$FILENAME
-    ln -s $DOTFILES_DIR/$FILENAME $TARGET_DIR/$FILENAME
+  local target_path=$1
+  if [[ -e $target_path && ! -L $target_path ]]; then
+    mv "$target_path" "${target_path}.bak"
   fi
 }
 
-link .aliases
-link .bashrc
-link .zshrc
-link .tmux.conf.local
-link .gitconfig
-link .gitignore_global
-link .inputrc
-link .vimrc
-link .wgetrc
-[[ ! -d $HOME/.config/kitty ]] && mkdir -p $HOME/.config/kitty && link kitty.conf $HOME/.config/kitty
-[[ ! -d $HOME/.config/helix ]] && mkdir -p $HOME/.config/helix && ln -s $DOTFILES_DIR/helix/config.toml $HOME/.config/helix
-echo "Choose NvChad or AstroNvim by enter a or n or empty to skip? (Please remove .config/nvim before this step)"
-echo -n "a/n:"
-read -r nvim
-if [[ $nvim == "n" ]]; then
-  [[ ! -d $HOME/.config/nvim ]] && \
-  echo "Installing NvChad" && \
-  rm -rf ~/.local/share/nvim ~/.local/state/nvim ~/.cache/nvim ; \
-  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && \
-  rm -rf $HOME/.config/nvim/lua/custom && \
-  ln -s $DOTFILES_DIR/nvchad/custom $HOME/.config/nvim/lua
-elif [[ $nvim == "a" ]]; then
-  [[ ! -d $HOME/.config/nvim ]] && \
-  rm -rf ~/.local/share/nvim ~/.local/state/nvim ~/.cache/nvim ; \
-  echo "Installing AstroNvim" && \
-  git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim && \
-  mkdir -p ~/.config/astronvim/lua/ ; \
-  ln -s $DOTFILES_DIR/astronvim ~/.config/astronvim/lua/user
+link() {
+  local filename=$1
+  local target_dir=${2:-$HOME}
+  mkdir -p "$target_dir"
+  local target_path="$target_dir/$filename"
+
+  if [[ ! -e $target_path ]]; then
+    echo "Linking $target_path"
+    ln -s "$DOTFILES_DIR/$filename" "$target_path"
+  elif [[ ! -L $target_path ]]; then
+    echo "Replacing $target_path"
+    backup "$target_path"
+    ln -s "$DOTFILES_DIR/$filename" "$target_path"
+  fi
+}
+
+config_link() {
+  local name=$1
+  local force=$2
+  local target_dir="$HOME/.config/$name"
+  local src_dir="$DOTFILES_DIR/config/$name"
+  local os_suffix
+
+  case "$(uname)" in
+    Darwin)
+      os_suffix=".mac" ;;
+    Linux)
+      os_suffix=".linux" ;;
+    MINGW* | MSYS* | CYGWIN* | Windows_NT)
+      os_suffix=".win" ;;
+  esac
+  [[ -d "$src_dir$os_suffix" ]] && src_dir="$src_dir$os_suffix"
+  [[ -d $target_dir && $force != "-f" ]] && return
+
+  if [[ $force == "-f" ]]; then
+    mkdir -p "$target_dir"
+    for item in "$src_dir"/*; do
+      local base_item=$(basename "$item")
+      local target_item="$target_dir/$base_item"
+      [[ -L $target_item ]] && continue
+      backup "$target_item"
+      ln -sf "$item" "$target_item"
+    done
+  else
+    [[ -d $target_dir ]] && return
+    ln -s "$src_dir" "$target_dir"
+  fi
+}
+
+dotfiles=(
+  .aliases
+  .bashrc
+  .zshrc
+  .tmux.conf.local
+  .gitconfig
+  .gitignore_global
+  .inputrc
+  .vimrc
+  .wgetrc
+)
+for file in "${dotfiles[@]}"; do
+  link "$file"
+done
+
+config_link kitty
+config_link helix
+config_link zed -f
+config_link alacritty
+config_link karabiner
+
+if [[ ! -d $HOME/.config/nvim ]]; then
+    echo "Choose NvChad or AstroNvim by entering a, n, or empty to skip"
+    read -rp "a/n: " nvim
+
+    case $nvim in
+    n)
+        echo "Installing NvChad"
+        rm -rf $HOME/.local/share/nvim $HOME/.local/state/nvim ~/.cache/nvim
+        git clone --depth 1 https://github.com/NvChad/NvChad $HOME/.config/nvim
+        rm -rf $HOME/.config/nvim/lua/custom
+        ln -s "$DOTFILES_DIR/config/nvim/nvchad/custom" $HOME/.config/nvim/lua
+        ;;
+    a)
+        echo "Installing AstroNvim"
+        rm -rf $HOME/.local/share/nvim $HOME/.local/state/nvim $HOME/.cache/nvim
+        git clone --depth 1 https://github.com/AstroNvim/AstroNvim $HOME/.config/nvim
+        mkdir -p $HOME/.config/astronvim/lua/
+        ln -s "$DOTFILES_DIR/config/nvim/astronvim" $HOME/.config/astronvim/lua/user
+        ;;
+    esac
 fi
 
 [ -d $HOME/.termux ] && link termux.properties $HOME/.termux && wget -O ~/.termux/colors.properties https://raw.githubusercontent.com/dracula/termux/master/colors.properties
